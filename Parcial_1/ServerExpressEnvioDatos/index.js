43185f974905d0ba157bcae3f8bb5898c3404e4b
CabraLuis <zetinaluis03@gmail.com>
2024-02-09 19:38:41 -0600

    sisis

--- Parcial_1/ServerExpressEnvioDatos/index.js
+++ Parcial_1/ServerExpressEnvioDatos/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.text());
+app.use(express.urlencoded({ extended: false }));
 
 // Rutas
 
@@ -23,13 +24,17 @@ app.get("/maestros/:carrera", (req, res) => {
 });
 
 // Parametros por body
-app.get("/administrativos/", (req, res) => {
+app.get("/administrativos", (req, res) => {
   // console.log(req.body);
-
   for (const key in req.body) {
     console.log(req.body[key]);
   }
+  res.send("Peticion POST exitosa");
+});
 
+// Parametros por formulario
+app.get("/prefectos", (req, res) => {
+  console.log(req.body);
   res.send("Peticion POST exitosa");
 });
 
