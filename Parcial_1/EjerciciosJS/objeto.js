a38f4f01fdfb02d2895e0f17c0a6c04d4e9eb69f
CabraLuis <zetinaluis03@gmail.com>
2024-02-16 09:47:39 -0600

    ejerciciosJS

---
+++ Parcial_1/EjerciciosJS/objeto.js
@@ -0,0 +1,13 @@
+alumno = {
+  nroControl: "20100173",
+  nombre: "Luis",
+  apellido: "Cabra",
+};
+
+// tomar peticion y actualizar en bd
+
+// declarar objetos y construir cadena de update en sql
+
+for (const dato in alumno) {
+  console.log(alumno[dato]);
+}
