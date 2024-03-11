e1f1ddcb8734cd39e0cd7eaefb7fab58955cb781
Luis Cabra <zetinaluis03@gmail.com>
2024-03-11 07:29:20 -0500

    router y sqlite

--- Parcial_2/ServerRouter/routes/rutaJuegos.js
+++ Parcial_2/ServerRouter/routes/rutaJuegos.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const rutaJuegos = express.Router();
 
+const sqlite = require("sqlite3");
+const db = new sqlite.Database("../../juegos.db");
+
 rutaJuegos.get("/", (req, res, next) => {
-  res.send("Peticion get");
+  var id = parseInt(req.query.id);
+  var query = "SELECT * FROM juego" + (id ? ` WHERE id = ${id}` : "");
+  db.all(query, [], (rows) => {
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
 });
+
 rutaJuegos.post("/", (req, res, next) => {
-  res.send("Peticion post");
+  var id = parseInt(req.query.id);
+  var query = `INSERT INTO juegos (nombre, anio, desarrolladora, imagen) VALUES ()`;
+  db.all(query, [], (rows) => {
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
 });
+
 rutaJuegos.patch("/", (req, res, next) => {
   res.send("Modificar");
 });
+
 rutaJuegos.delete("/", (req, res, next) => {
-  res.send("Borrar");
+  var id = parseInt(req.query.id);
+  var query = `DELETE FROM juego WHERE id = ${id}`;
+  db.all(query, [], (rows) => {
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
 });
 
-module.exports.rutaJuegos = rutaJuegos;
+module.exports = rutaJuegos;
