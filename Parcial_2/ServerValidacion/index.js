e828ae25037a4a275e060caf15c26a1a12d11b8d
CabraLuis <zetinaluis03@gmail.com>
2024-03-21 21:16:08 -0500

    validacion

--- Parcial_2/ServerValidacion/index.js
+++ Parcial_2/ServerValidacion/index.js
@@ -1,11 +1,9 @@
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const express = require("express");
+const rutaJuegos = require("./routes/rutaJuegos.js");
 const app = express();
 
-app.post("/upload", upload.single("image"), (req, res, next) => {
-  res.send("Subida exitosa");
-});
+app.use(express.json());
+app.use("/juegos", rutaJuegos);
 
 app.use((req, res, next) => {
   res.status(404).send("URL not found");
---
+++ Parcial_2/ServerValidacion/routes/rutaJuegos.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const rutaJuegos = express.Router();
+const { query, validationResult, check } = require("express-validator");
+
+const db = require("better-sqlite3")("../../juegos.db");
+
+rutaJuegos.use(express.json());
+
+rutaJuegos.get(
+  "/",
+  query("id").notEmpty().isNumeric().escape(),
+  (req, res, next) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const row = db
+        .prepare(`SELECT * FROM juego WHERE id = ?`)
+        .get(req.query.id);
+
+      return res.send(row);
+    }
+    res.send({ errors: result.array() });
+  }
+);
+
+rutaJuegos.post(
+  "/",
+  check(["nombre", "desarrolladora", "imagen"]).notEmpty().isString().escape(),
+  check("anio").notEmpty().isInt().isLength(4),
+  (req, res, next) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const query = db
+        .prepare(
+          `INSERT INTO juego (nombre, anio, desarrolladora, imagen) VALUES (?, ?, ?, ?)`
+        )
+        .run(
+          req.body.nombre,
+          req.body.anio,
+          req.body.desarrolladora,
+          req.body.imagen
+        );
+
+      return res.status(200).send("Juego agregado exitosamente");
+    }
+    res.send({ errors: result.array() });
+  }
+);
+
+rutaJuegos.patch("/", (req, res, next) => {
+  res.send("Responde PATCH");
+});
+
+rutaJuegos.delete("/", (req, res, next) => {
+  res.send("Responde DELETE");
+});
+module.exports = rutaJuegos;
